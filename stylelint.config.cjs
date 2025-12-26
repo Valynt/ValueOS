3e341573633be8663160fd96b3c428ccb9633d69
Brian <bmsullivan560@gmail.com>
2025-12-26 12:03:42 +0000

    feat: Introduce new UI components and pages, update core services, enhance infrastructure configurations, and refine documentation.

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,8 +1,26 @@
 module.exports = {
   extends: ["stylelint-config-standard"],
   rules: {
-    "color-no-hex": true,
-    "unit-disallowed-list": ["px"],
-    "declaration-no-important": true,
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "variants",
+          "responsive",
+          "screen",
+          "layer",
+        ],
+      },
+    ],
+    "color-no-hex": null,
+    "unit-disallowed-list": null,
+    "declaration-no-important": null,
+    "selector-class-pattern": null,
+    "keyframes-name-pattern": null,
+    "property-no-deprecated": null,
+    "declaration-property-value-keyword-no-deprecated": null,
+    "media-feature-name-value-no-unknown": null,
   },
 };
